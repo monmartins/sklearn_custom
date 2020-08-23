e41fb374f8ddd0379e6d495066ef09b7a4f3bb0a
Ramon Martins <ramonmartins.campos@gmail.com>
2020-08-23 18:03:20 -0300

    add RandomForestRegressorCustom

--- drop_sklearn_transforms/sklearn_transformers.py
+++ drop_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,6 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn import preprocessing
+from sklearn.ensemble import RandomForestRegressor
 
 class RemoveZerosRows(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
@@ -25,8 +26,8 @@ class RemoveZerosRows(BaseEstimator, TransformerMixin):
 class LabelEncoderColumns(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
         self.columns = columns
-        # self.label = preprocessing.LabelEncoder()
-        # self.encode = True
+        self.label = preprocessing.LabelEncoder()
+        self.encode = True
 
     def fit(self, X, y=None):
         return self
@@ -34,13 +35,40 @@ class LabelEncoderColumns(BaseEstimator, TransformerMixin):
     def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
-        # if(self.encode == True):
-        #     data[self.columns] = self.label.fit_transform(data[self.columns])
-        #     self.encode = False
-        # else:
-        #     data[self.columns] = self.label.inverse_transform(data[self.columns])
-        #     self.encode = True
+        if(self.encode == True):
+            data[self.columns] = self.label.fit_transform(data[self.columns])
+            self.encode = False
+        else:
+            data[self.columns] = self.label.inverse_transform(data[self.columns])
+            self.encode = True
         return data
+
+class RandomForestRegressorCustom(BaseEstimator, TransformerMixin):
+    def __init__(self, columns, max_depth=5, random_state=0):
+        self.columns = columns
+        self.max_depth = max_depth
+        self.random_state = random_state
+        self.label = preprocessing.LabelEncoder() 
+        self.regr = RandomForestRegressor(self.max_depth, self.random_state)
+
+    def fit(self, X, y=None):
+        
+        if y != None:
+            y2_train = self.label.fit_transform(y) 
+        else:
+            y2_train = self.label.fit_transform(X[self.columns]) 
+            
+        self.regr.fit(X,y2_train)
+        return self.regr
+    
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        
+        return data
+    def predict(self, X):
+        y_pred = self.regr.predict(X)
+        return self.label.inverse_transform(y_pred)
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
